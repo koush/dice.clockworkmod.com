b4a9ce96c8528081bd7ab5ce04bac68617f38971
Koushik Dutta <koushd@gmail.com>
2018-06-14 23:10:01 -0700

    index fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
   $('#add-btn').click(function(e) {
     var add = $("<input class='form-control expression' id='expression' placeholder='d20'>");
-    add.val(Cookies.get('expression-' + $('#adds').length) || 'd20');
+    add.val(Cookies.get('expression-' + $('.expression').length) || 'd20');
     $('#adds').prepend(add);
   })
 
